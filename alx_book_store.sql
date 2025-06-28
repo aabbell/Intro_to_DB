0d6695a8d9ae201634d3c6944ddecf446eb0dec5
aabbell <abelalemneh68@gmail.com>
2025-06-28 14:02:04 +0300

    fIrst

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -32,9 +32,9 @@ CREATE TABLE Orders (
 CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
-    FOREIGN KEY(order_id) REFERENCES(order_id),
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
     book_id INT,
-    FOREIGN KEY(book_id) REFERENCES(book_id),
+    FOREIGN KEY(book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
 
