2e4b9116c34683cbdf59033ca79a7c36f23c1221
aabbell <abelalemneh68@gmail.com>
2025-06-28 14:30:34 +0300

    fIrst

--- MySQLServer.py
+++ MySQLServer.py
@@ -14,7 +14,7 @@ try:
     mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
 except:
-    mysql.connector.Error as err:
+    mysql.connector.Error:
     print(f"Error:{err}")
 
 finally:
